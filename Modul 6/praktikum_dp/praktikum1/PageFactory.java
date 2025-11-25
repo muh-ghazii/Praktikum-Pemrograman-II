66a22c646bbb480034bdddd27ac9176e45cf17d3
Muhammad Ghazi Rakhmadi <mghazirakhmadi63@gmail.com>
2025-11-26 07:55:17 +0800

    Delete Modul 6/praktikum_dp directory

--- Modul 6/praktikum_dp/praktikum1/PageFactory.java
+++
@@ -1,8 +0,0 @@
-package praktikum1;
-// Design Pattern (Creational): FACTORY METHOD
-// - Mendefinisikan interface untuk membuat objek Page berdasarkan key.
-public interface PageFactory {
-
-    // Factory Method: dipanggil client (CLIApp) untuk membuat halaman.
-    Page createPage(String key);
-}
